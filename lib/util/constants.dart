c669fdd89a026ab679846d4559846869c4f864b5
rajmohan1584 <105063220+rajmohan1584@users.noreply.github.com>
2023-03-03 17:37:58 -0500

    mcast ip port change

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -1,6 +1,6 @@
 class CONST {
   static String iPhoneIP = "192.168.50.250";
   static String macBookIP = "192.168.50.181";
-  static String multicastIP = "239.255.255.255";
-  static int multicastPort = 4048;
+  static String multicastIP = "224.1.2.3";
+  static int multicastPort = 4567;
 }
