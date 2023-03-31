69ad9264335e2efdd1544dc6219ab6a43b228897
rajmohan1584 <105063220+rajmohan1584@users.noreply.github.com>
2023-03-30 21:49:42 -0400

    Quiz Master login works.

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,5 @@
+import 'package:buzzer/server/server.dart';
+import 'package:buzzer/util/log.dart';
 import 'package:buzzer/util/widgets.dart';
 import 'package:flutter/material.dart';
 
@@ -9,7 +11,102 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  onLogin() {}
+  final FocusNode focusNode = FocusNode();
+  bool qm = false;
+  final maxlen = 6;
+  String passkey = "";
+  final secretKey = "135246";
+
+  void gotoServer() {
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (BuildContext context) => const BuzzServerScreen()));
+  }
+
+  onTextChanged(String s) {
+    setState(() {
+      passkey = s;
+      if (passkey.length == maxlen) {
+        if (passkey == secretKey) {
+          // success - goto server
+          gotoServer();
+        } else {
+          // reset
+          passkey = "";
+          qm = false;
+        }
+      }
+    });
+    Log.log(s);
+  }
+
+  onLogin() {
+    setState(() {
+      qm = true;
+    });
+    Future.delayed(const Duration(milliseconds: 100), () {
+      // Do something
+      FocusScope.of(context).requestFocus(focusNode);
+    });
+  }
+
+  Widget passwordInput() {
+    const green = Color(0xff82fb4c);
+    final boxes = <Widget>[];
+    for (var i = 0; i < maxlen; i++) {
+      final len = passkey.length;
+      Color? fillColor;
+      if (i < len) fillColor = green;
+      boxes.add(Container(
+        width: 50,
+        height: 50,
+        decoration: BoxDecoration(
+          color: fillColor,
+          borderRadius: BorderRadius.circular(50),
+          border: Border.all(color: Colors.black, width: 2),
+          boxShadow: const [
+            BoxShadow(
+              color: green,
+              blurRadius: 30,
+            ),
+          ],
+        ),
+      ));
+    }
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: boxes,
+    );
+  }
+
+  Widget messageOrInput(w) {
+    if (qm) {
+      return SizedBox(
+        width: w / 2,
+        height: 50,
+        child: TextField(
+            focusNode: focusNode,
+            autofocus: true,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            keyboardType: TextInputType.number,
+            onChanged: onTextChanged,
+            style: const TextStyle(fontSize: 32, color: Color(0xff82fb4c))),
+      );
+    } else {
+      return SizedBox(
+          width: w,
+          height: 50,
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: const [
+            Text("Waiting For Quiz Master",
+                style: TextStyle(fontSize: 32, color: Color(0xff82fb4c)))
+          ]));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     double w = MediaQuery.of(context).size.width;
@@ -17,12 +114,23 @@ class _HomeState extends State<Home> {
       w = MediaQuery.of(context).size.height;
     }
     w -= 20;
+
+    Widget? fab;
+    if (!qm) {
+      fab = FloatingActionButton.extended(
+          onPressed: onLogin, label: const Text("Login as Quiz Master"));
+    }
+
     return Scaffold(
         appBar: AppBar(title: const Text("Buzzer - Waiting for Quiz Master")),
         backgroundColor: Colors.black,
-        floatingActionButton: FloatingActionButton.extended(
-            onPressed: onLogin, label: const Text("Login as Quiz Master")),
+        floatingActionButton: fab,
         body: Center(
-            child: SizedBox(width: w, height: w, child: WIDGETS.buildRadar())));
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          messageOrInput(w),
+          SizedBox(height: 20),
+          SizedBox(width: w, height: w, child: WIDGETS.buildRadar())
+        ])));
   }
 }
